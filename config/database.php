7b5a90ed2d6abfb1bc35572141eae87c2488596a
Lucas Monteiro <lucasggtyy358@gmail.com>
2025-08-14 13:48:53 -0300

    Configurando conexão com banco de dados

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+// Configuração do banco de dados MySQL
+define('DB_HOST', '18.228.17.85');// IP do seu servidor de banco de dados
+define('DB_USER', 'User');       //Nome do banco
+define('DB_NAME', 'Database'); // O nome do banco de dados
+define('DB_PASS', 'database123'); // A senha do usuário
+
+?>

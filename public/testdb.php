2b8497d74ff7b46fb98618ff3b30a3e6ef634e98
Lucas Monteiro <lucasggtyy358@gmail.com>
2025-08-14 23:38:48 -0300

    Ajustando configs do mysql e testdb

--- public/testdb.php
+++ public/testdb.php
@@ -6,7 +6,7 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 
 if ($conn->connect_error) {
-    die("<h2>❌ Falha na conexão: " . $conn->connect_error . "</h2>");
+    die("<h2> Falha na conexão: " . $conn->connect_error . "</h2>");
 }
 
 echo "<h2> Conexão com o banco de dados bem-sucedida!</h2>";
